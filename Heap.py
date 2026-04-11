576b17edf31a1543a50f8a52aed5f9fe73b28d44
seunghyun <seunghyeong311@gmail.com>
2026-04-11 19:50:34 +0900

    upload Heap impl

---
+++ Heap.py
@@ -0,0 +1,53 @@
+class Heap:
+    def __init__(self, *args):
+        if len(args) != 0:
+            self.__A = args[0]  # 파라미터로 온 리스트
+        else:
+            self.__A = []
+
+    def insert(self, x):
+        self.__A.append(x)
+        self.__percolateUp(len(self.__A) - 1)
+
+    def __percolateUp(self, i: int):
+        parent = (i - 1) // 2
+        if i > 0 and self.__A[i] > self.__A[parent]:
+            self.__A[i], self.__A[parent] = self.__A[parent], self.__A[i]
+            self.__percolateUp(parent)
+
+    def deleteMax(self):
+        if self.isEmpty():
+            return None
+        if len(self.__A) == 1:
+            return self.__A.pop()   # 대입 없이 pop()만!
+        max_val = self.__A[0]
+        self.__A[0] = self.__A.pop()
+        self.__percolateDown(0)
+        return max_val
+
+    def __percolateDown(self, i: int):
+        # Percolate down w/ self.__A[i] as the root
+        child = 2 * i + 1  # left child
+        right = 2 * i + 2  # right child
+        if child <= len(self.__A) - 1:
+            if right <= len(self.__A) - 1 and self.__A[child] < self.__A[right]:
+                child = right  # index of larger child
+            if self.__A[i] < self.__A[child]:
+                self.__A[i], self.__A[child] = self.__A[child], self.__A[i]
+                self.__percolateDown(child)
+
+    def max(self):
+        return self.__A[0]
+
+    def buildHeap(self):
+        for i in range((len(self.__A) - 2) // 2, -1, -1):
+            self.__percolateDown(i)
+
+    def isEmpty(self) -> bool:
+        return len(self.__A) == 0
+
+    def clear(self):
+        self.__A = []
+
+    def size(self) -> int:
+        return len(self.__A)
